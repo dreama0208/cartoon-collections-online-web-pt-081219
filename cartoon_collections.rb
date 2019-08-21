7019a4ca4180b177b9f3b05521b0147bba8dded9
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-21 23:51:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,9 @@ def summon_captain_planet(arr)# code an argument here
   }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  arr.any? { |e| e.length > 4  }
 end
 
 def find_the_cheese# code an argument here
