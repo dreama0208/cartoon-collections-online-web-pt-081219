5eebccaf9ac241e09c7f077f023bdc5a4438cd18
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-21 23:40:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each.with_index(1) { |name, index|
+      puts "#{index} #{name}"
+  }
 end
 
 def summon_captain_planet# code an argument here
