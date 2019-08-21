5679d6ed3fb51be9a49bd310e1c73cea6c876962
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-21 23:44:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)# code an argument here
   # Your code here
+  arr.collect{ |i| 
+      i.upcase + "!"
+  }
 end
 
 def long_planeteer_calls# code an argument here
