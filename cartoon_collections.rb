f30ff46715cc896389729636574bbf8418374333
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-22 00:00:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(arr)# code an argument here
   arr.any? { |e| e.length > 4  }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   arr.find { |e| cheese_types.include?(e) == true }
